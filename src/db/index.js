d113c88c4e534fc0f070cda41b03ea86c375d0f8
Jayesh Badgujar <badgujarjayesh491@gmail.com>
2026-07-16 22:30:37 +0530

    added exit code

--- src/db/index.js
+++ src/db/index.js
@@ -9,6 +9,7 @@ const connectDB = async () => {
     console.log(`Connected to MongoDB: ${connectionInstance.connection.host}`);
   } catch (error) {
     console.error("Error connecting to MongoDB:", error.message);
+    process.exit(1);
   }
 };
 
