f991e8fef1a2bf2ec4bc328243f154d5729fcafc
Jayesh Badgujar <badgujarjayesh491@gmail.com>
2026-07-19 15:36:13 +0530

    feat: add cookie-parser and cors middleware for enhanced request handling

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import dns from "node:dns";
 import dotenv from "dotenv";
 import { fileURLToPath } from "url";
 import connectDB from "./db/index.js";
+import app from "./app.js";
 
 dns.setServers(["1.1.1.1", "8.8.8.8"]);
 
@@ -12,9 +13,16 @@ dotenv.config({
   path: envPath
 });
 
-(async () => {
-    await connectDB();
-})();
+    await connectDB()
+    .then(() => {
+      app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port ${process.env.PORT || 8000 }`);
+      })
+    })
+    .catch((error) => {
+        console.error("Error connecting to MongoDB:", error);
+    });
+
 
 /*
 (async () => {
